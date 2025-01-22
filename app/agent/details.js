3153f60b9fa7dcdc3b014fb1a9aaa67ab2cbd574
Algo Sphere <105270904+algosphere-ai@users.noreply.github.com>
2025-01-23 02:40:35 +0530

    initial commit

---
+++ app/agent/details.js
@@ -0,0 +1,16 @@
+const agentDetails = {
+    name: "AlgoSphere", // token name
+    symbol: "ALGO", // token symbol
+    description: "An AI Agent on Pumpfun.", // token description
+    initialBuyAmount: 0.001,
+    // personality (customise your AI agent)
+    personality: "Crypto Analyst",
+    // image
+    imagePath: "app/agent/logo.png",
+    // socials
+    twitter: "https://twitter.com/example",
+    telegram: "https://t.me/example",
+    website: "https://example.com",
+};
+
+export { agentDetails };
