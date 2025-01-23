186d93507ee62f4c49fe3baa246d124c32abf81f
Steve Adams <150558724+steveadams08@users.noreply.github.com>
2025-01-23 21:10:19 +0530

    add formatPrice, utils js refac

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -1,4 +1,4 @@
-function formatMarketcap(value) {
+export const formatMarketcap = (value) => {
     const num = parseFloat(value);
     if (num >= 1_000_000) {
         return `${(num / 1_000_000).toFixed(0)}M`;
@@ -9,4 +9,13 @@ function formatMarketcap(value) {
     return num.toFixed(0);
 }
 
-export { formatMarketcap };
+export const formatPrice = (value) => {
+    const num = parseFloat(value);
+    if (num >= 1_000_000) {
+        return `${(num / 1_000_000).toFixed(2)}M`;
+    }
+    else if (num >= 1_000) {
+        return `${(num / 1_000).toFixed(2)}K`;
+    }
+    return num.toFixed(2);
+}
